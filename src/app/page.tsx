44991efc667f45841649d966758422e5b340462f
Lakmal thathsara <it21277986@my.sliit.lk>
2025-02-05 01:43:24 +0530

    Remove unused Image import from Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import Hero from "@/components/Hero/Hero";
 import ToggleButton from "@/components/Item/ToggleButton";
-import Image from "next/image";
 
 export default function Home() {
   return (
